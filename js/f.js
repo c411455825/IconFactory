46c6683cc3b5412f807ec081cd120ae5117bc0e3
liuhong <525754781@qq.com>
2013-05-13 17:45:59 +0800

    create

---
+++ js/f.js
@@ -0,0 +1,16 @@
+(function(){
+    function A(){
+        var t = this;
+        t.sourcePath = "source";
+    }
+    var B = A.prototype;
+    B.init = function(){
+
+    };
+    B.initCanvas = function(){
+
+    };
+    B.readIcon = function(){
+
+    };
+})();
